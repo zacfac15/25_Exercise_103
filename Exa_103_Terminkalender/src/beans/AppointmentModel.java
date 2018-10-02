53627f2cb427e860e963fe9eafa544fea6eb24d8
Fabian Zach <zacfac15@new.edvhtl.at>
2018-10-02 09:23:19 +0200

    Added All ActionEvents in the GUI

---
+++ Exa_103_Terminkalender/src/beans/AppointmentModel.java
@@ -0,0 +1,20 @@
+package beans;
+
+import javax.swing.AbstractListModel;
+
+public class AppointmentModel extends AbstractListModel
+{
+
+  @Override
+  public int getSize()
+  {
+    
+  }
+
+  @Override
+  public Object getElementAt(int index)
+  {
+    
+  }
+  
+}
